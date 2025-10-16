42dad3620d210eb41425bff59ed1d20e441d309f
JassimMHD <jassimjr10you@gmail.com>
2025-10-16 14:19:58 +0530

    Link added

--- Components/AdminComponents/Sidebar.jsx
+++ Components/AdminComponents/Sidebar.jsx
@@ -7,7 +7,9 @@ const Sidebar = () => {
   return (
     <div className="flex flex-col bg-slate-100">
       <div className="px-2 sm:pl-14 py-3 border border-black">
-        <Image src={assets.logo} width={120} alt="" />
+        <Link href="/">
+          <h1 className="text-3xl font-semibold p-2">Blogger</h1>
+        </Link>
       </div>
       <div className="w-28 sm:w-80 h-[100vh] relative py-12 border border-black">
         <div className="w-[50%] sm:[80%] absolute right-0"></div>
@@ -23,14 +25,6 @@ const Sidebar = () => {
           className=" mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white"
         >
           <Image src={assets.blog_icon} alt="" width={28} /> <p>Blog lists</p>
-        </Link>
-
-        <Link
-          href="/admin/subscriptions"
-          className="mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white"
-        >
-          <Image src={assets.email_icon} alt="" width={28} />{" "}
-          <p>Subscriptions</p>
         </Link>
       </div>
     </div>
